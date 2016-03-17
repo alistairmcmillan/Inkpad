dbb372a317eca1bace243f2217b2657489dfd221
Alistair McMillan <alistairmcmillan75@gmail.com>
2016-03-17 01:22:11 +0000

    Oops. Should have been the key not the secret.

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # For info: http://docs.buddybuild.com/v3.0/docs/custom-prebuild-and-postbuild-steps
 
-/usr/libexec/PlistBuddy -c "Add CFBundleURLTypes:0:CFBundleURLSchemes:0 string db-\$DropboxConsumerSecret" Inkpad-Info.plist
+/usr/libexec/PlistBuddy -c "Add CFBundleURLTypes:0:CFBundleURLSchemes:0 string db-\$DropboxConsumerKey" Inkpad-Info.plist
